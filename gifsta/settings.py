96af8b1254f39699fe8b393d91c139f729f932d6
DenysGurin <gurindenys@gmail.com>
2017-03-19 22:30:03 +0200

    Login

--- gifsta/settings.py
+++ gifsta/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
+    
     'social_django.middleware.SocialAuthExceptionMiddleware',   
 ]
 
